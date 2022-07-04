ca408a402128c37ef0c25cafdcfab145d94f8a2f
Haris_ali90 <haris.ali@joeyco.com>
2022-07-04 15:21:07 +0000

    init

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ app.get('/', function(request, response) {
 //     res.sendFile(__dirname + '/index.html');
 // });
 
-const io = require("socket.io")(http, {
-    cors: {
-        origin:'*',
-        withCredentials: true,
-        methods: ["GET"],
-        transports: ["websocket", "polling"],
-    },
-    allowEIO3: true,
-});
+// const io = require("socket.io")(http, {
+//     cors: {
+//         origin:'*',
+//         withCredentials: true,
+//         methods: ["GET", "POST"],
+//         transports: ["websocket", "polling"],
+//     },
+//     allowEIO3: true,
+// });
 
 
 // const io = require('socket.io')(http, {
@@ -38,15 +38,15 @@ const io = require("socket.io")(http, {
 //     allowEIO3: true,
 // });
 
-io.on('connection', function (socket) {
-    socket.on( 'new_notification', function( data ) {
-        io.sockets.emit( 'show_notification', {
-            hubId: data.hubId,
-            title: data.title,
-            message: data.message,
-        });
-    });
-});
+// io.on('connection', function (socket) {
+//     socket.on( 'new_notification', function( data ) {
+//         io.sockets.emit( 'show_notification', {
+//             hubId: data.hubId,
+//             title: data.title,
+//             message: data.message,
+//         });
+//     });
+// });
 //
 // http.listen(app.get('port'), function() {
 //     console.log('listening on localhost:3000');
