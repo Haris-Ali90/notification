38e73653817326bca376c7a5e01e25843f09600b
Haris_ali90 <haris.ali@joeyco.com>
2022-07-04 12:06:31 +0000

    init

--- index.js
+++ index.js
@@ -14,21 +14,21 @@ app.get('/', function(request, response) {
 // });
 
 
-// const io = require('socket.io')(http, {
-//     cors: {
-//         origin: '*',
-//     },
-// });
-//
-// io.on('connection', function (socket) {
-//     socket.on( 'new_notification', function( data ) {
-//         io.sockets.emit( 'show_notification', {
-//             hubId: data.hubId,
-//             title: data.title,
-//             message: data.message,
-//         });
-//     });
-// });
+const io = require('socket.io')(http, {
+    cors: {
+        origin: '*',
+    },
+});
+
+io.on('connection', function (socket) {
+    socket.on( 'new_notification', function( data ) {
+        io.sockets.emit( 'show_notification', {
+            hubId: data.hubId,
+            title: data.title,
+            message: data.message,
+        });
+    });
+});
 //
 // http.listen(app.get('port'), function() {
 //     console.log('listening on localhost:3000');
