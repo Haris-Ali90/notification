830342f3ed1b7bc8136daf55884ff97252e6dbac
Haris_ali90 <haris.ali@joeyco.com>
2022-07-05 07:48:16 +0000

    init

--- index.js
+++ index.js
@@ -6,23 +6,21 @@ app.set('port', (process.env.PORT || 3000));
 
 app.get('/', function(request, response) {
     response.sendFile(__dirname + '/index.html');
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
 });
 
 // app.get('/', (req, res) => {
 //     res.sendFile(__dirname + '/index.html');
 // });
 
-const io = require("socket.io")(http, {
-    cors: {
-        origin:'*',
-        withCredentials: true,
-        methods: ["GET", "POST"],
-        transports: ["websocket", "polling"],
-    },
-    allowEIO3: true,
-});
+// const io = require("socket.io")(http, {
+//     cors: {
+//         origin:'*',
+//         withCredentials: true,
+//         methods: ["GET", "POST"],
+//         transports: ["websocket", "polling"],
+//     },
+//     allowEIO3: true,
+// });
 
 
 // const io = require('socket.io')(http, {
@@ -38,16 +36,21 @@ const io = require("socket.io")(http, {
 //     allowEIO3: true,
 // });
 
-// io.on('connection', function (socket) {
-//     socket.on( 'new_notification', function( data ) {
-//         io.sockets.emit( 'show_notification', {
-//             hubId: data.hubId,
-//             title: data.title,
-//             message: data.message,
-//         });
-//     });
-// });
+io.on('connection', function (socket) {
+    socket.on( 'new_notification', function( data ) {
+        io.sockets.emit( 'show_notification', {
+            hubId: data.hubId,
+            title: data.title,
+            message: data.message,
+        });
+    });
+});
 //
+
 // http.listen(app.get('port'), function() {
-//     console.log('listening on localhost:3000');
-// });
+//     console.log('App is running, server is listening on port ', app.get('port'));
+// })
+
+http.listen(app.get('port'), function() {
+    console.log('listening on localhost:3000');
+});
